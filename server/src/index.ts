4eb994337de78ba11fe5c3005a3adf5d8f056f05
Sugam Ghising <ghisingsugam04@gmail.com>
2025-11-01 11:19:10 +0545

    refactor: simplify CORS configuration and enhance root endpoint response

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,16 +15,17 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000", // your React frontend
-    credentials: true, // allow cookies
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tours', tourRoutes);
 app.use('/api/bookings', bookingRoutes);
+
+app.get('/', (req, res) => {
+    return res.send("Backend running");
+})
 
 app.use(errorHandler)
 
